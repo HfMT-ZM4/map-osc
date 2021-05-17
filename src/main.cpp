0a7a1ca2135bd5b4a70939cc2816af2a3324c521
ramagottfried <rama.gottfried@gmail.com>
2021-05-17 19:01:18 +0200

    added documentation in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,32 +6,84 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     
-    /*
-    OSCAtomVector vec;
-    
-    vec.appendValue(1.1);
-    vec.appendValue("foo");
 
-    vec.print();
-    
-    OSCAtomVector vec2;
-    
-    vec2.appendValue( vec );
-    
-    vec2.print();
-
-*/
     OSCMap bndl, bndl2;
 
-    bndl2.addMessage("/bar", "hei", 2, 3, 4 );
-    bndl2.addMessage("/self_sub", bndl2);
-    bndl.addMessage("/foo", bndl2);
+    // adding a message
+    bndl.addMessage("/float", (float)1.1);
+    bndl.addMessage("/double", (double)2.2);
+    bndl.addMessage("/int", (int32_t)10);
+    bndl.addMessage("/long", (int64_t)20);
+    bndl.addMessage("/string", "hello osc!");
+    
+    // adding a message with a list
+    bndl.addMessage("/bar", "hei", 2, 3, 4 );
+    
+    // creating an empty message
+    bndl2.addMessage("/accumuate");
     
     
-    bndl2.print();
-   // cout << bndl["/foo"].get<string>() << endl;
+    for( int i = 0; i < 10; i++ )
+    {
+        // appending values to make a list iterably
+        bndl2["/accumuate"]->appendValue(i);
+        // note that the messages are pointers
+    }
+    
+    // adding a subbbundle
+    bndl.addMessage("/sub", bndl2);
+    
+    // print
+    bndl.print();
+    
+    //
+    cout << "this is a float " << bndl["/float"]->get<float>() << endl;
+    
+    // by default get<>() returns the first element
+    cout << "this is the first element of /bar " << bndl["/bar"]->get<string>() << endl;
+    
+    cout << "this is the second element /bar " << bndl["/bar"]->get<string>(1) << endl;
+
+    cout << bndl["/float"]->size() << endl;
+    
+    // this will crash since we ask for an element that is outside the list size
+    // cout << "this is the second element /float " << bndl["/float"]->get<string>(1) << endl;
+
   
+    // example of writing into a std::string
+    string serialString = bndl.getSerializedString();
     
+    // test printing the bytes
+    /*
+    for( int i = 0; i < serialString.size(); i++ )
+    {
+       printf("%d 0x%x %d\n", i, (unsigned char)serialString[i], (unsigned char)serialString[i]);
+    }
+    */
+    
+    // example of reading an OSC encoded char array
+    OSCMap deserialized( serialString.length(), serialString.data() );
+    cout << "\n printing the deserialized bundle: \n " << endl;
+    deserialized.print();
+    
+   
+    /*
+    // example of writing into a C char array
+    size_t size = bndl.getSerializedSizeInBytes();
+    char * ptr = (char *)malloc(size);
+
+    bndl.serializeIntoBuffer(ptr, size);
+
+    for( int i = 0; i < size; i++ )
+    {
+       printf("%d 0x%x %d\n", i, (unsigned char)ptr[i], (unsigned char)ptr[i]);
+    }
+
+    free(ptr);
+     */
+
+    /*
+    // in development, figuring out a method for literal definition
     OSCMap test({
         {"/t", 1},
         {"/sub",
@@ -40,38 +92,9 @@ int main(int argc, const char * argv[]) {
             })
         }
     });
-    
-    
-    
-    
-    
-    /*
-    size_t size = bndl.getSerializedSizeInBytes();
-    char * ptr = (char *)malloc(size);
-    
-    bndl.serializeIntoBuffer(ptr, size);
-    
-    
-    for( int i = 0; i < size; i++ )
-    {
-        //printf("%i %c %u\n", i, ptr[i], (unsigned int)ptr[i] );
-        printf("%d 0x%x %d\n", i, (unsigned char)ptr[i], (unsigned char)ptr[i]);
-        //std::cout << i << " " << ptr[i] << " " << (unsigned int)ptr[i] << std::endl;
-    }
-    
-    free(ptr);
-
     */
-
-    string serialString = bndl.getSerializedString();
-    for( int i = 0; i < serialString.size(); i++ )
-    {
-       printf("%d 0x%x %d\n", i, (unsigned char)serialString[i], (unsigned char)serialString[i]);
-    }
     
-    OSCMap deserialized( serialString.length(), serialString.data() );
     
-   
-  //  deserialized.print();
+    
     return 0;
 }
